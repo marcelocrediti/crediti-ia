30e613341ba4a599bc724f598d6500bcbfb2367b
marcelocrediti <marcelinoteixeira.santos@gmail.com>
2026-09-07 14:01:34 -0300

    Preparar configuração central de educação

---
+++ src/config/education.js
@@ -0,0 +1,50 @@
+export const ESTACIO_URL =
+  "https://estacio.br/selecao?cod_agente=14369444&u=804215&end=1";
+
+export const EDUCATION_PARTNERS = [
+  {
+    id: "estacio",
+    name: "Estácio",
+    symbol: "◇",
+    category: "GRADUAÇÃO, PÓS E CURSOS",
+    title: "Escolha seu próximo passo",
+    action: "CONHECER CURSOS ›",
+    url: ESTACIO_URL
+  },
+  {
+    id: "wyden",
+    name: "Wyden",
+    symbol: "W",
+    category: "GRADUAÇÃO, PÓS E CURSOS TÉCNICOS",
+    title: "Formação para transformar sua carreira",
+    action: "CONHECER CURSOS ›",
+    url: "https://wyden.com.br/selecao?cod_agente=14369444&u=805310&end=1"
+  },
+  {
+    id: "uninter",
+    name: "Uninter",
+    symbol: "U",
+    category: "PRESENCIAL, SEMIPRESENCIAL E EAD",
+    title: "Estude com flexibilidade onde estiver",
+    action: "VER OPÇÕES DE CURSOS ›",
+    url: "https://fichadeinscricao.uninter.com/Inscricoes/Hash/70DgPqJpvjeEXJznyJXfu44e744BN1zPIXnEKsZszmFvRv1J-jy9YsxcfRbl17W0EpcUpmrhwFI"
+  },
+  {
+    id: "unifatecie",
+    name: "UniFatecie",
+    symbol: "UF",
+    category: "GRADUAÇÃO, PÓS E SEGUNDA GRADUAÇÃO",
+    title: "Escolha sua formação presencial ou EAD",
+    action: "CONHECER CURSOS ›",
+    url: "https://afiliado.saberemrede.net/services?type=D&institution=UNIFATECIE%20-%20Consultores&campaign=unifatecie&&buyer=94410&campaign=unifatecie&shared=true"
+  },
+  {
+    id: "idomed",
+    name: "IDOMED",
+    symbol: "+",
+    category: "MEDICINA, ODONTOLOGIA E PÓS",
+    title: "Formação para cuidar e transformar vidas",
+    action: "CONHECER CURSOS ›",
+    url: "https://www.idomed.com.br/selecao?cod_agente=14369444&u=805477"
+  }
+];
